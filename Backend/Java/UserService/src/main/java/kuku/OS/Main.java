a7fa13ec8921436aed33fe0b8ebcf6dec9055ab9
kukupie123 <kuchukboromd@gmail.com>
2024-01-27 15:41:26 +0530

    userService can now invoke DBService endpoint

--- Backend/Java/UserService/src/main/java/kuku/OS/Main.java
+++ Backend/Java/UserService/src/main/java/kuku/OS/Main.java
@@ -5,41 +5,65 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import io.github.acm19.aws.interceptor.http.AwsRequestSigningApacheInterceptor;
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import com.google.gson.Gson;
+import kuku.OS.model.ResponseModel;
+import kuku.OS.service.APICallService;
+import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.signer.Aws4Signer;
 import software.amazon.awssdk.regions.Region;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 
+    private final Gson gson = new Gson();
+
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+
+
         /**
          * Important Notes
          * We can use STS to get session credentials
          * DefaultCredentialsProvider will assume the role attached to it. so make sure your attached role has the right permissions set.
          */
-        String endpoint = "https://7fkgoc5qr4.execute-api.ap-south-1.amazonaws.com/DEV/private/db-service/users";
+        String dbEndpoint = "https://7fkgoc5qr4.execute-api.ap-south-1.amazonaws.com/DEV/private/db-service/users";
+        APICallService callService = APICallService.getInstance();
 
+        String method = requestEvent.getHttpMethod().toUpperCase();
 
-        HttpRequestInterceptor interceptor = new AwsRequestSigningApacheInterceptor("execute-api", Aws4Signer.create(), DefaultCredentialsProvider.create(), Region.AP_SOUTH_1);
-        HttpPost httpPost = new HttpPost(endpoint);
-        try (CloseableHttpClient closeableHttpClient = HttpClients.custom().addInterceptorLast(interceptor).build()) {
-            CloseableHttpResponse httpResponse = closeableHttpClient.execute(httpPost);
-            String responseString = EntityUtils.toString(httpResponse.getEntity());
-            return new APIGatewayProxyResponseEvent().withBody(responseString);
-        } catch (IOException e) {
-            return new APIGatewayProxyResponseEvent().withBody(e.getMessage()).withStatusCode(500);
+        String body = requestEvent.getBody();
+        Map bodyMap = gson.fromJson(body, Map.class);
+
+        switch (method) {
+            case "POST": {
+                String userId = (String) bodyMap.get("userId");
+                String password = (String) bodyMap.get("password");
+                try {
+                    Map<String, String> payloadMap = new HashMap<>();
+                    payloadMap.put("action", "user.getUserByIdAndPassword");
+                    payloadMap.put("userId", userId);
+                    payloadMap.put("password", password);
+                    String payload = gson.toJson(payloadMap);
+                    HttpResponse response = callService.invokeAWSEndpoint(dbEndpoint, "execute-api", Region.AP_SOUTH_1, payload, null);
+                    String responseString = EntityUtils.toString(response.getEntity());
+                    Map respponseMap = gson.fromJson(responseString, Map.class);
+                    if (response.getStatusLine().getStatusCode() != 200) {
+                        return sendResponse(response.getStatusLine().getStatusCode(), gson.toJson(new ResponseModel<>((String) respponseMap.get("msg"), null)));
+                    }
+                    //TODO: Return JWT TOKEN
+                } catch (IOException e) {
+                    return sendResponse(500, gson.toJson(new ResponseModel<>(e.getMessage() + "\n" + e.getStackTrace(), null)));
+                }
+            }
         }
+        return sendResponse(404, gson.toJson(new ResponseModel<>("WTF HAPPENED", null)));
+    }
 
+    private APIGatewayProxyResponseEvent sendResponse(int statusCode, String body) {
+        return new APIGatewayProxyResponseEvent().withStatusCode(statusCode).withBody(body);
     }
 }
---
+++ Backend/Java/UserService/src/main/java/kuku/OS/service/APICallService.java
@@ -0,0 +1,60 @@
+package kuku.OS.service;
+
+import io.github.acm19.aws.interceptor.http.AwsRequestSigningApacheInterceptor;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.signer.Aws4Signer;
+import software.amazon.awssdk.regions.Region;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class APICallService {
+
+    private static APICallService instance;
+
+    private APICallService() {
+    }
+
+    public static APICallService getInstance() {
+        if (instance == null) {
+            instance = new APICallService();
+        }
+        return instance;
+    }
+
+    /**
+     * Invoke an AWS Endpoint with AWS4 Signature. The Credential provider will assume the role attached to it using "DefaultCredentialProvider.create()"
+     *
+     * @param endpoint
+     * @param serviceName
+     * @param region
+     * @return
+     * @throws IOException
+     */
+    public HttpResponse invokeAWSEndpoint(String endpoint, String serviceName, Region region, String body, Map<String, String> headers) throws IOException {
+        HttpRequestInterceptor interceptor = new AwsRequestSigningApacheInterceptor(serviceName, Aws4Signer.create(), DefaultCredentialsProvider.create(), region);
+        HttpPost httpPost = new HttpPost(endpoint);
+
+        // Set body
+        StringEntity bodyEntity = new StringEntity(body, ContentType.APPLICATION_JSON);
+        httpPost.setEntity(bodyEntity);
+
+        if (headers != null)
+            // Set headers
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                httpPost.addHeader(entry.getKey(), entry.getValue());
+            }
+
+        CloseableHttpClient closeableHttpClient = HttpClients.custom().addInterceptorLast(interceptor).build();
+        org.apache.http.HttpResponse httpResponse = closeableHttpClient.execute(httpPost);
+
+        return httpResponse;
+    }
+}
